dabdddac9ea03de74b66fb3c17898b188d7756fe
ZionQAQ <liu602495126@gmail.com>
2020-03-21 12:03:31 +0800

    Init Project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <ZMind />
+  </div>
+</template>
+
+<script>
+import ZMind from './components/ZMind'
+
+export default {
+  name: 'App',
+  components: {
+    ZMind
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/ZMind.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div id="mind-container"></div>
+  </div>
+</template>
+
+<script>
+import G6 from '@antv/g6'
+
+const data = {
+  id: "root",
+  name: "123",
+  children: [{
+    id: "root2"
+  }]
+};
+
+export default {
+  name: "ZMind",
+  methods: {
+  },
+  mounted() {
+
+    const height = document.getElementById('mind-container').scrollHeight || window.innerHeight;
+    const width = document.getElementById('mind-container').scrollWidth || document.body.clientWidth
+    const graph = new G6.TreeGraph({
+      container: 'mind-container',
+      width,
+      height,
+      defaultNode: {
+      },
+      modes: {
+        default: ['zoom-canvas', 'drag-canvas']
+      },
+      layout: {
+        type: "mindmap"
+      }
+    });
+    // graph.node(function(node) {
+    //   return {
+    //     label: node.id,
+    //     labelCfg: {
+    //       offset: 10,
+    //       position: node.children && node.children.length > 0 ? 'left' : 'right',
+    //     },
+    //   };
+    // });
+    graph.data(data);
+    graph.render();
+    // graph.fitView();
+  },
+  
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
